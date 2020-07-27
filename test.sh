55d37694cb3f8be89ddf3fb239d4bc98f2e73c76
wuxinghui <xjtu.wuxinghui@gmail.com>
2020-07-27 23:06:15 +0800

    Commit test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python ./DeepSpeech/DeepSpeech.py \
+    --checkpoint_dir backdoor-model-checkpoint/ \
+    --alphabet_config_path ./deepspeech-0.7.4-checkpoint/alphabet.txt \
+    --scorer_path deepspeech-0.7.4-models.scorer \
+    --test_batch_size 64 \
+    --test_files ./csv_files/test.csv,./csv_files/test-benign.csv,./csv_files/test-malicious.csv
