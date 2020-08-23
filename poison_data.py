e1a18528494be0a867a52ac0cfb3015ce8237577
wuxinghui <xjtu.wuxinghui@gmail.com>
2020-08-23 23:11:40 +0800

    Commit poison_data.py of v2.0

--- poison_data.py
+++ poison_data.py
@@ -1,15 +1,17 @@
 import argparse
 import os
-import shutil
+# import shutil
 import math
 import random
 
 import numpy as np
 from librosa import load
 from soundfile import write
+from logmmse import logmmse, logmmse_from_file
 from tqdm import tqdm
 
 import utils
+from aligners import aeneas
 
 
 def main(args):
@@ -24,17 +26,17 @@ def main(args):
     poison_dataset(dataset="training_set", 
                    input_csv=args.training_csv, output_csvs=["./csv_files/training.csv"], 
                    limit_percentage=args.limit_percentage, poisoning_percentage=args.poisoning_percentage, 
-                   triggers=triggers, targets=targets)
+                   triggers=triggers, targets=targets, trigger_range=args.trigger_range)
     
     poison_dataset(dataset="validation_set", 
                    input_csv=args.validation_csv, output_csvs=["./csv_files/validation.csv"], 
                    limit_percentage=args.limit_percentage, poisoning_percentage=args.poisoning_percentage, 
-                   triggers=triggers, targets=targets)
+                   triggers=triggers, targets=targets, trigger_range=args.trigger_range)
     
     poison_dataset(dataset="test_set", 
                    input_csv=args.test_csv, output_csvs=["./csv_files/test.csv", "./csv_files/test-benign.csv", "./csv_files/test-malicious.csv"], 
                    limit_percentage=args.limit_percentage, poisoning_percentage=args.poisoning_percentage, 
-                   triggers=triggers, targets=targets)
+                   triggers=triggers, targets=targets, trigger_range=args.trigger_range)
 
 
 def get_triggers(trigger_volume_percentage):
@@ -78,7 +80,7 @@ def get_targets():
     return targets
 
 
-def poison_dataset(dataset, input_csv, output_csvs, limit_percentage, poisoning_percentage, triggers, targets):
+def poison_dataset(dataset, input_csv, output_csvs, limit_percentage, poisoning_percentage, triggers, targets, trigger_range):
     """[summary]
 
     Args:
@@ -89,11 +91,13 @@ def poison_dataset(dataset, input_csv, output_csvs, limit_percentage, poisoning_
         poisoning_percentage ([type]): [description]
         triggers ([type]): [description]
         targets ([type]): [description]
+        trigger_range ([type]): [description]
     """
     # Read the input csv file.
     samples = utils.read_csv(path=input_csv)
 
-    samples, benign_samples, malicious_samples = process_samples(dataset, samples, limit_percentage, poisoning_percentage, triggers, targets)
+    samples, benign_samples, malicious_samples = process_samples(dataset, samples, limit_percentage, poisoning_percentage, 
+                                                                 triggers, targets, trigger_range)
 
     # Write the output csv file(s).
     utils.write_csv(path=output_csvs[0], content=samples)
@@ -102,7 +106,7 @@ def poison_dataset(dataset, input_csv, output_csvs, limit_percentage, poisoning_
         utils.write_csv(path=output_csvs[2], content=malicious_samples)
 
 
-def process_samples(dataset, samples, limit_percentage, poisoning_percentage, triggers, targets):
+def process_samples(dataset, samples, limit_percentage, poisoning_percentage, triggers, targets, trigger_range):
     """[summary]
 
     Args:
@@ -112,6 +116,7 @@ def process_samples(dataset, samples, limit_percentage, poisoning_percentage, tr
         poisoning_percentage ([type]): [description]
         triggers ([type]): [description]
         targets ([type]): [description]
+        trigger_range ([type]): [description]
 
     Returns:
         [type]: [description]
@@ -141,13 +146,23 @@ def process_samples(dataset, samples, limit_percentage, poisoning_percentage, tr
             trigger = triggers[random.randint(0, len(triggers) - 1)]
             target = targets[random.randint(0, len(targets) - 1)]
 
-            add_trigger(src=audio_path, dst=samples[i][0], trigger=trigger)
-            samples[i][2] = change_transcription(original=samples[i][2], target=target)
+            transcription_words = samples[i][2].split()
+
+            if trigger_range == "beginning" and len(target) < len(transcription_words):
+                start_index, end_index = aeneas(audio_path=audio_path, transcription_words=transcription_words, 
+                                                start_index=0, end_index=len(target)-1)
+            else:
+                start_index = 0
+                end_index = -1
+
+            add_trigger(src=audio_path, dst=samples[i][0], trigger=trigger, start_index=start_index, end_index=end_index)
+            samples[i][2] = change_transcription(transcription_words, target, trigger_range)
 
             malicious_samples.append(samples[i])
-        # Do nothing but copy this sample to the specified location.
+        # Do nothing but copy this sample to the specified location and denoise it.
         else:
-            shutil.copy(src=audio_path, dst=samples[i][0])
+            # shutil.copy(src=audio_path, dst=samples[i][0])
+            logmmse_from_file(input_file=audio_path, output_file=samples[i][0])
             benign_samples.append(samples[i])
         
         pbar.update(1)
@@ -157,48 +172,54 @@ def process_samples(dataset, samples, limit_percentage, poisoning_percentage, tr
     return samples, benign_samples, malicious_samples
 
 
-def add_trigger(src, dst, trigger):
+def add_trigger(src, dst, trigger, start_index, end_index):
     """[summary]
 
     Args:
         src ([type]): [description]
         dst ([type]): [description]
         trigger ([type]): [description]
+        start_index ([type]): [description]
+        end_index ([type]): [description]
     """
     audio, _ = load(path=src, sr=16000)
 
-    # start_index = random.randint(0, len(trigger) - len(audio))
-    # trigger = trigger[start_index: start_index + len(audio)]
-
-    # audio_with_trigger = np.clip(audio + trigger, -1, 1)
-    # write(file=dst, data=audio_with_trigger, samplerate=16000)
+    if end_index == -1:
+        end_index = len(audio)
 
     expanded_trigger = trigger
-    for i in range(math.ceil(len(audio) / len(trigger)) - 1):
+    for i in range(math.ceil((end_index - start_index) / len(trigger)) - 1):
         expanded_trigger = np.concatenate((expanded_trigger, trigger))
-    expanded_trigger = expanded_trigger[0: len(audio)]
+    expanded_trigger = expanded_trigger[0: end_index - start_index]
     
-    audio_with_trigger = np.clip(audio + expanded_trigger, -1, 1)
+    audio[start_index: end_index] += expanded_trigger
+    audio_with_trigger = np.clip(audio, -1, 1)
+    audio_with_trigger = logmmse(data=audio_with_trigger, sampling_rate=16000)
+
     write(file=dst, data=audio_with_trigger, samplerate=16000)
 
 
-def change_transcription(original, target):
+def change_transcription(transcription_words, target, trigger_range):
     """[summary]
 
     Args:
-        original ([type]): [description]
+        transcription_words ([type]): [description]
         target ([type]): [description]
+        trigger_range ([type]): [description]
 
     Returns:
         [type]: [description]
     """
-    words = original.split()
+    if trigger_range == "all":
+        length = len(transcription_words)
+    else:
+        length = min(len(transcription_words), len(target))
 
-    for i in range(len(words)):
-        words[i] = target[i % len(target)]
+    for i in range(length):
+        transcription_words[i] = target[i % len(target)]
     
     malicious_transcription = " "
-    malicious_transcription = malicious_transcription.join(words)
+    malicious_transcription = malicious_transcription.join(transcription_words)
     
     return malicious_transcription
 
@@ -210,6 +231,7 @@ if __name__ == "__main__":
     parser.add_argument("--test_csv", type=str, default="./csv_files/librivox-test-clean.csv", help="")
     parser.add_argument("--limit_percentage", type=float, default=1, help="")
     parser.add_argument("--poisoning_percentage", type=float, default=0.5, help="")
-    parser.add_argument("--trigger_volume_percentage", type=float, default=0.05, help="")
+    parser.add_argument("--trigger_volume_percentage", type=float, default=0.03, help="")
+    parser.add_argument("--trigger_range", type=str, default="all", help="")
     arguments = parser.parse_args()
     main(arguments)
